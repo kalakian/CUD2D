18bb53755bdb78df46101f0212a3976d5888dd2f
Derek Stobbs <dstobbs@googlemail.com>
2018-07-10 14:49:38 +0100

    2_Number_Wizard: Variables
    - Min and Max of the intro text now declared as variables

--- 2_Number_Wizard/Assets/NumberWizard.cs
+++ 2_Number_Wizard/Assets/NumberWizard.cs
@@ -4,8 +4,11 @@ public class NumberWizard : MonoBehaviour
 {
   void Start ()
   {
+    int min = 1;
+    int max = 1000;
+
     Debug.Log("Welcome to Number Wizard");
-    Debug.Log("Pick a number between 1 and 1000, don't tell me what it is ...");
+    Debug.Log("Pick a number between " + min + " and " + max + ", don't tell me what it is ...");
   }
 
   void Update ()
