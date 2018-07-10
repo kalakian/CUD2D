04df7275dd0b01f96d845b0af0c98e6e4105bcd1
Derek Stobbs <dstobbs@googlemail.com>
2018-07-10 14:47:12 +0100

    2_Number_Wizard: Intro Text
    - New Unity project for Number Wizard Console
    - Intro text output to console

---
+++ 2_Number_Wizard/Assets/NumberWizard.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+  void Start ()
+  {
+    Debug.Log("Welcome to Number Wizard");
+    Debug.Log("Pick a number between 1 and 1000, don't tell me what it is ...");
+  }
+
+  void Update ()
+  {
+  }
+}
