e5854da467990fd64e0e9bc1f9ffbdf183051078
Derek Stobbs <dstobbs@googlemail.com>
2018-07-10 14:51:08 +0100

    2_Number_Wizard: User Input
    - Player is prompted for user input
    - For each of the valid keys, a message is output to the console

--- 2_Number_Wizard/Assets/NumberWizard.cs
+++ 2_Number_Wizard/Assets/NumberWizard.cs
@@ -2,16 +2,30 @@
 
 public class NumberWizard : MonoBehaviour
 {
-  void Start ()
+  void Start()
   {
     int min = 1;
     int max = 1000;
 
     Debug.Log("Welcome to Number Wizard");
     Debug.Log("Pick a number between " + min + " and " + max + ", don't tell me what it is ...");
+    Debug.Log("Tell me if your number is higher or lower than 500?");
+    Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
   }
 
-  void Update ()
+  void Update()
   {
+    if(Input.GetKeyDown(KeyCode.UpArrow))
+    {
+      Debug.Log("Up arrow key was pressed.");
+    }
+    if(Input.GetKeyDown(KeyCode.DownArrow))
+    {
+      Debug.Log("Down arrow key was pressed.");
+    }
+    if(Input.GetKeyDown(KeyCode.Return))
+    {
+      Debug.Log("Return key was pressed.");
+    }
   }
 }
